e7f29d421c0a55dba4d384244a2ce118376f832b
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-03-04 07:47:51 +0000

    Generate the Favorite model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_220739) do
+ActiveRecord::Schema.define(version: 2021_03_04_074422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "house_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["house_id"], name: "index_favorites_on_house_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "houses", force: :cascade do |t|
     t.string "name", null: false
@@ -32,4 +41,6 @@ ActiveRecord::Schema.define(version: 2021_03_03_220739) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "favorites", "houses"
+  add_foreign_key "favorites", "users"
 end
