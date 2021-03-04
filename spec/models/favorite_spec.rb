8a659b0b4e045fc6d65e3aac3537721ea9b2c124
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-03-04 08:06:40 +0000

    Add an association/relation between houses and the favorites table

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -4,4 +4,7 @@ RSpec.describe Favorite, type: :model do
   # Validation test
   it { should(validate_presence_of(:user_id)) }
   it { should(validate_presence_of(:house_id)) }
+
+  # Association test
+  it { should(belong_to(:house)) }
 end
