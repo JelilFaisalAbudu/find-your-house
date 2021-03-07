1118cf8d36ac97fc7d7b296f0acaadd88a90e161
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-03-07 22:38:17 +0000

    Set up JWT

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module FindYourHouse
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Load files by specifying the lib folder as one of the folders in the list of RoR _autoload_s
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end
