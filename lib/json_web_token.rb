d30e1ce187e815e0a9faf9cd56ed6767caf0a1cd
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-03-08 00:09:17 +0000

    Add JWT error handler helper method

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,7 +2,7 @@ class JsonWebToken
   SECRET_KEY = Rails.application.credentials.secret_key_base.to_s
 
   def self.encode(payload, exp = 24.hours.from_now)
-    payload[:exp] = exp
+    payload[:exp] = exp.to_i
 
     JWT.encode(payload, SECRET_KEY)
   end
@@ -10,5 +10,11 @@ class JsonWebToken
   def self.decode(token)
     decoded = JWT.decode(token, SECRET_KEY).first
     HashWithIndifferentAccess.new decoded
+
+    # rescue from all decode errors
+    rescue JWT::DecodeError => e
+      # raise custom error to be handled by custom handler
+      raise ExceptionHandler::InvalidToken, e.message
+    end
   end
 end
