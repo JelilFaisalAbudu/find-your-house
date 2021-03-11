4dc365a1841c24e18894dcbd8afcfd5877d88965
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-03-11 09:58:00 +0000

    Update the Favorites controller

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,22 +1,15 @@
 class Api::V1::FavoritesController < ApplicationController
-  before_action :set_user, only: %i[index create]
   before_action :set_favorite, only: %i[destroy]
-  before_action :check_login, only: %i[create]
   before_action :check_owner, only: %i[destroy]
 
   def index
-    @user_favorites = @user.favorites
-    json_response(@user_favorites)
+    current_user_favorites = current_user.favorites
+    json_response(current_user_favorites)
   end
 
   def create
-    @favorite = @user.favorites.build(house_id: params[:house_id])
-    if @favorite.save
-      json_response(@favorite, :created)
-    else
-
-      json_response({ errors: @favorites.errors }, :unprocessable_entity)
-    end
+    @favorite = current_user.favorites.create!(house_id: params[:house_id])
+    json_response(@favorite, :created)
   end
 
   def destroy
@@ -25,10 +18,6 @@ class Api::V1::FavoritesController < ApplicationController
   end
 
   private
-
-  def set_user
-    @user = User.find(params[:user_id])
-  end
 
   def set_favorite
     @favorite = Favorite.find(params[:id])
